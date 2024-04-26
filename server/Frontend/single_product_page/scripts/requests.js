e1db16009140435e37923ce489b0a477f275ee4a
NightApelsin <izmsasha58@gmail.com>
2024-04-26 20:45:26 +0300

    добовление логики перехода к конкретному товару из БД

---
+++ server/Frontend/single_product_page/scripts/requests.js
@@ -0,0 +1,16 @@
+﻿export async function getSingleProduct(id){
+    try{
+        const response = await fetch(`/catalog/:${id}`,{
+            method: 'GET'
+        })
+        if(!response.ok){
+            throw new Error("Network ERROR" + response.status)
+        }
+        const result = await response.json();
+        console.log(result)
+        return result
+    }catch (error){
+        console.error('Error fetching products:', error);
+        throw error;
+    }
+}
--- server/Routers/router.js
+++ server/Routers/router.js
@@ -4,6 +4,7 @@ const productController = require('../Controllers/product.controller.js')
 
 
 router.get('/catalog', productController.getAllProducts)
+router.get('/catalog/:id', productController.getOneProduct)
 
 
 module.exports = router;
