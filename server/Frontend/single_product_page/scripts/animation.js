e80f62d344a34acd2d4c95b0adf85db398c98699
Александр <izmsasha58@gmail.com>
2024-05-03 20:09:17 +0300

    исправление непоняток с GIT и добавление переходов по харрактеристикам

--- server/Frontend/single_product_page/scripts/animation.js
+++ server/Frontend/single_product_page/scripts/animation.js
@@ -1,11 +1,11 @@
 function soHardMenuTopBarSwitch(event, tabId) {
     let tablinks = document.getElementsByClassName("btn-primary");
-    
+
     for (let i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     event.target.classList.toggle('active')
-    
+
     let tabs = document.getElementsByClassName("hard-item");
     for (let i = 0; i < tabs.length; i++){
         tabs[i].className = tabs[i].className.replace(" hard-active", "");
@@ -13,5 +13,16 @@ function soHardMenuTopBarSwitch(event, tabId) {
     document.querySelector(`${tabId}`).classList.toggle('hard-active');
 }
 function subHardMenuSwitcher(event, tabId){
-    
+    let tablinks = document.getElementsByClassName("param-category-link");
+
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    event.target.classList.toggle('active')
+
+    let tabs = document.getElementsByClassName("param-category-display");
+    for (let i = 0; i < tabs.length; i++){
+        tabs[i].className = tabs[i].className.replace(" active", "");
+    }
+    document.querySelector(`${tabId}`).classList.toggle('active');
 }
