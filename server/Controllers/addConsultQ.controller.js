f61df0af46c233220554169889c64f8cbd02e31c
Александр <izmsasha58@gmail.com>
2024-05-16 02:10:29 +0300

    добавлено первое модальное окно

---
+++ server/Controllers/addConsultQ.controller.js
@@ -0,0 +1,17 @@
+﻿const db = require('../Database/db.js');
+
+class ConsultQ {
+    async addConsulQ(req, res) {
+        const phone = req.body
+        let cookies = req.cookies.historyPath || {path: []};
+        try{
+            await db.query('insert into qforconsultation ("clientPhone", "clientCookie") values ($1, $2) returning *',
+                [phone.phoneNumber, cookies])
+            
+        }catch (e){
+            res.sendStatus(400)
+        }
+        res.sendStatus(200)
+    }
+}
+module.exports = new ConsultQ()
--- server/Frontend/index/scripts/main_page_script.js
+++ server/Frontend/index/scripts/main_page_script.js
@@ -1,4 +1,29 @@
-﻿$(document).ready(function() {
+﻿
+
+document.addEventListener('DOMContentLoaded',function() {
+    //modal window
+    console.log(document.querySelectorAll('.openModalBtn'))
+    document.querySelectorAll(".openModalBtn").forEach(e=>{
+        console.log(e)
+        e.addEventListener('click',function() {
+        console.log('a')
+        $("#myModal").dialog({
+            modal: true,
+            height: 340,
+            width: 600,
+            reliable: false,
+            draggable: false,
+            beforeClose: function( event, ui ) {
+                document.querySelector('body').style.overflow = 'scroll'
+            },
+            open: function( event, ui ) {
+                document.querySelector('body').style.overflow = 'hidden';
+            }
+        });
+
+        
+        })
+    });
     //nav-bar menu
     $('.burger-button').click(function(event) {
         $('.burger-button, .burger-list-container').toggleClass('active');
@@ -33,7 +58,55 @@
 })
 
 
+async function addFetch(){
+    if(document.querySelector(`input[placeholder='Телефон']`).classList.contains('error')){
+        return 0
+    }else{
+        let formData = {phoneNumber:document.querySelector(`input[placeholder='Телефон']`).value}
+        console.log(formData)
+        try {
+            let response = await fetch('/api/addPersonOnConsultQueue/', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if(!response.ok){
+                console.log('something failed')
+            }else {
+                document.querySelector('#complete').classList.add('enabled')
+                document.querySelector('.ui-resizable-handle.ui-resizable-s').classList.add('width-0');
+                setTimeout(() => {
+                    document.querySelector('#complete').classList.remove('enabled')
+                    document.querySelector('.ui-resizable-handle.ui-resizable-s').classList.remove('width-0');
+                    $('#myModal').dialog('close')
+                }, 5000)
+            }
 
+        }catch (e){
+            console.error('There has been a problem with your fetch operation:', e);
+        }
+        // }).then(data=>{
+        //
+        //     document.querySelector('#complete').classList.add('enabled')
+        //     document.querySelector('.ui-resizable-handle.ui-resizable-s').classList.add('width-0');
+        //     setTimeout(()=>{
+        //         document.querySelector('#complete').classList.remove('enabled')
+        //         document.querySelector('.ui-resizable-handle.ui-resizable-s').classList.remove('width-0');
+        //         $('#myModal').dialog('close')
+        //     }, 5000)
+        // }).then(response => {
+        //     if (!response.ok) {
+        //         throw new Error('Network response was not ok');
+        //     }
+        //     return response.json();
+        // }).catch(error => {
+        //     console.error('There has been a problem with your fetch operation:', error);
+        // })
+
+    }
+}
 
 
 
